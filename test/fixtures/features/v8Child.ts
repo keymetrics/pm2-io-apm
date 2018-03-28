f405558bfbe7bf59ed4109f67b839afacf8995b3
vince <wallet77@gmail.com>
2018-03-28 15:55:03 +0200

    feature: add config and default value for services + fix test

--- test/fixtures/features/v8Child.ts
+++ test/fixtures/features/v8Child.ts
@@ -2,7 +2,7 @@ import V8Metric from '../../../src/metrics/v8'
 import Metric from '../../../src/features/metrics'
 
 const metric = new Metric()
-metric.init({v8: 'all'})
+metric.init({v8: 'all', eventLoopDelay: false})
 
 process.on('SIGINT', function () {
   metric.destroy()
