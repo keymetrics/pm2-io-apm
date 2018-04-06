6bd089d4ca750f50729a8f5d93691cde869f16b6
vince <wallet77@gmail.com>
2018-04-06 18:02:02 +0200

    feature: add heap dump + heap sampling action (+ tests)

---
+++ src/actions/profilingHeap.ts
@@ -0,0 +1,73 @@
+import debug from 'debug'
+debug('axm:profilingaction')
+
+import ActionsFeature from '../features/actions'
+import ProfilingFeature from '../features/profiling'
+
+export default class ProfilingHeapAction {
+
+  private actionFeature: ActionsFeature
+  private profilingFeature
+
+  constructor (actionFeature: ActionsFeature) {
+    this.actionFeature = actionFeature
+    this.profilingFeature = new ProfilingFeature().init()
+    this.profilingFeature.heapProfiling.init()
+  }
+
+  exposeActions () {
+
+    // -------------------------------------
+    // Heap sampling
+    // -------------------------------------
+    this.actionFeature.action('km:heap:sampling:start', async (reply) => {
+      try {
+        await this.profilingFeature.heapProfiling.start()
+        reply({ success : true })
+      } catch (err) {
+        return reply({
+          success: false,
+          err : err
+        })
+      }
+
+    })
+
+    this.actionFeature.action('km:heap:sampling:stop', async (reply) => {
+      try {
+        const dumpFile = await this.profilingFeature.heapProfiling.stop()
+
+        return reply({
+          success     : true,
+          heapdump  : true,
+          dump_file   : dumpFile
+        })
+      } catch (err) {
+        return reply({
+          success: false,
+          err : err
+        })
+      }
+    })
+
+    // -------------------------------------
+    // Heap dump
+    // -------------------------------------
+    this.actionFeature.action('km:heapdump', async (reply) => {
+      try {
+        const dumpFile = await this.profilingFeature.heapProfiling.takeSnapshot()
+
+        return reply({
+          success     : true,
+          heapdump    : true,
+          dump_file   : dumpFile
+        })
+      } catch (err) {
+        return reply({
+          success: false,
+          err : err
+        })
+      }
+    })
+  }
+}
---
+++ test/fixtures/actions/profilingHeapChild.ts
@@ -0,0 +1,8 @@
+import Action from '../../../src/features/actions'
+import ProfilingHeap from '../../../src/actions/profilingHeap'
+
+const action = new Action()
+action.init()
+
+const profiling = new ProfilingHeap(action)
+profiling.exposeActions()
