2af927bbe3fd6e7bbb7fbd31fc239540fa170382
vince <wallet77@gmail.com>
2018-04-23 16:10:15 +0200

    feature: add file size and unique id in profiling reply

--- src/actions/profilingCpu.ts
+++ src/actions/profilingCpu.ts
@@ -4,11 +4,14 @@ debug('axm:profilingaction')
 import ActionsFeature from '../features/actions'
 import ProfilingFeature from '../features/profiling'
 import ActionsInterface from './actionsInterface'
+import MiscUtils from '../utils/miscellaneous'
+import FileUtils from '../utils/file'
 
 export default class ProfilingCPUAction implements ActionsInterface {
 
   private actionFeature: ActionsFeature
   private profilingFeature
+  private uuid: string
 
   constructor (actionFeature: ActionsFeature) {
     this.actionFeature = actionFeature
@@ -24,12 +27,14 @@ export default class ProfilingCPUAction implements ActionsInterface {
 
     this.actionFeature.action('km:cpu:profiling:start', async (reply) => {
       try {
+        this.uuid = MiscUtils.generateUUID()
         await this.profilingFeature.cpuProfiling.start()
-        reply({ success : true })
+        reply({ success : true, uuid: this.uuid })
       } catch (err) {
         return reply({
-          success: false,
-          err : err
+          success : false,
+          err     : err,
+          uuid    : this.uuid
         })
       }
 
@@ -39,15 +44,25 @@ export default class ProfilingCPUAction implements ActionsInterface {
       try {
         const dumpFile = await this.profilingFeature.cpuProfiling.stop()
 
+        let size
+        try {
+          size = await FileUtils.getFileSize(dumpFile)
+        } catch (err) {
+          size = -1
+        }
+
         return reply({
           success     : true,
           cpuprofile  : true,
-          dump_file   : dumpFile
+          dump_file   : dumpFile,
+          dump_file_size : size,
+          uuid        : this.uuid
         })
       } catch (err) {
         return reply({
-          success: false,
-          err : err
+          success : false,
+          err     : err,
+          uuid    : this.uuid
         })
       }
     })
