5321cdf0638922b1eeccd580043d29dd0c5cd982
vmarchaud <contact@vmarchaud.fr>
2018-04-25 16:43:57 +0200

    profiling: format new cpu profile to old format

--- src/profiling/profilingCPU.ts
+++ src/profiling/profilingCPU.ts
@@ -1,9 +1,11 @@
 import debug from 'debug'
 debug('axm:profiling')
 import ProfilingType from './profilingType'
+import { CustomProfile } from './profilingType'
 import FileUtils from '../utils/file'
 import { ServiceManager } from '../serviceManager'
 import { InspectorService } from '../services/inspector'
+import * as inspector from 'inspector'
 
 export default class ProfilingCPU implements ProfilingType {
 
@@ -32,16 +34,78 @@ export default class ProfilingCPU implements ProfilingType {
     return await this.getProfileInfo()
   }
 
+  private _convertTimeDeltas(profile: CustomProfile) {
+    if (!profile.timeDeltas)
+      return null;
+    let lastTimeUsec = profile.startTime;
+    const timestamps = new Array(profile.timeDeltas.length + 1);
+    for (let i = 0; i < profile.timeDeltas.length; ++i) {
+      timestamps[i] = lastTimeUsec;
+      lastTimeUsec += profile.timeDeltas[i];
+    }
+    timestamps[profile.timeDeltas.length] = lastTimeUsec;
+    return timestamps;
+  }
+
   private getProfileInfo () {
     return new Promise( async (resolve, reject) => {
-      let data
       try {
-        data = await this.inspectorService.post('Profiler.stop')
+        let rawData: any = await this.inspectorService.post('Profiler.stop')
+
+        if (!rawData || !rawData.profile) return reject(`V8 Interval Error`)
+        rawData = rawData.profile
+
+        let data: CustomProfile = rawData as CustomProfile
+
+        // recursively reformat the flatten tree into an actual tree
+        const reformatNode = node => {
+          if (!node.children) node.children = []
+
+          node.children = node.children.map(childID => {
+            if (typeof childID !== 'number') return childID
+            const childNode = data.nodes.find(node => node.id === childID)
+            if (typeof childNode !== 'object') return null
+            childNode.callUID = node.id
+            return childNode
+          })
+          return {
+            functionName: node.callFrame.functionName,
+            url: node.callFrame.url,
+            lineNumber: node.callFrame.lineNumber,
+            callUID: node.callUID,
+            bailoutReason: "",
+            id: node.id,
+            scriptId: 0,
+            hitCount: node.hitCount,
+            children: node.children.map(reformatNode)
+          }
+        }
+
+        // reformat then only keep the root as top level node
+        const nodes = data.nodes
+          .map(reformatNode)
+          .filter(node => node.functionName === '(root)' )[0]
+
+        // since it can be undefined, create an array so execution still works
+        if (!data.timeDeltas) {
+          data.timeDeltas = []
+        }
+
+        return resolve(FileUtils.writeDumpFile(JSON.stringify({
+          head: nodes,
+          typeId: 'CPU',
+          uid: '1',
+          startTime: Math.floor(data.startTime / 1000000),
+          title: "km-cpu-profiling",
+          endTime: Math.floor(data.endTime/ 1000000),
+          samples: data.samples,
+          timestamps: this._convertTimeDeltas(data)
+        })))
+
       } catch (err) {
         debug('Cpu profiling stopped !')
         return reject(err)
       }
-      return resolve(FileUtils.writeDumpFile(JSON.stringify(data.profile)))
     })
   }
 }
--- src/profiling/profilingType.ts
+++ src/profiling/profilingType.ts
@@ -1,6 +1,17 @@
+
+import * as inspector from 'inspector'
+
 export default interface ProfilingType {
   init (): void
   start (): void
   stop (): void
   destroy (): void
+}
+
+export interface CustomProfileNode extends inspector.Profiler.ProfileNode {
+    callUID: number
+}
+
+export interface CustomProfile extends inspector.Profiler.Profile {
+  nodes: CustomProfileNode[];
 }
