25a215d63520c47c041e2010124a06a48ae0a84c
vince <wallet77@gmail.com>
2018-04-26 11:28:52 +0200

    fix: don't force true in profiling conf if property exists

--- src/index.ts
+++ src/index.ts
@@ -184,12 +184,12 @@ class PMX {
       const networkConf: any = {}
 
       if (config.hasOwnProperty('network')) {
-        networkConf.traffic = config.network
+        networkConf.traffic = Boolean(config.network)
         delete config.network
       }
 
       if (config.hasOwnProperty('ports')) {
-        networkConf.ports = config.ports
+        networkConf.ports = Boolean(config.ports)
         delete config.ports
       }
 
@@ -254,8 +254,8 @@ class PMX {
       this.initActionsConf(config)
 
       config.actions = {
-        profilingCpu: true,
-        profilingHeap: true
+        profilingCpu: config.profiling,
+        profilingHeap: config.profiling
       }
       delete config.profiling
     }
