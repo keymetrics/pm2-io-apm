3f5795efc3f2f4374a3f7a08ead54a459274e90a
vince <wallet77@gmail.com>
2018-05-02 15:47:43 +0200

    test: add tests on proxy utils

---
+++ test/utils/proxy.spec.ts
@@ -0,0 +1,12 @@
+import Proxy from '../../src/utils/proxy'
+import { expect } from 'chai'
+import * as chai from 'chai'
+import 'mocha'
+
+describe('Proxy', () => {
+
+  it('should return debug function in case method is not found', () => {
+    const res = Proxy.wrap({}, ['toto'], {})
+    expect(typeof res).to.equal('function')
+  })
+})
