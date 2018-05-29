6794e51b2d639039bc8cb724099152672d00847c
vince <wallet77@gmail.com>
2018-05-29 14:25:16 +0200

    fix: #100 fix max listeners warning

--- test/fixtures/apiActionsScopedChild.ts
+++ test/fixtures/apiActionsScopedChild.ts
@@ -3,3 +3,7 @@ import SpecUtils from './utils'
 const pmx = require(__dirname + '/../../src/index.js')
 
 pmx.scopedAction('testScopedAction', function (opts, res) { res.send('testScopedActionReply') })
+
+process.on('SIGINT', function () {
+  pmx.destroy()
+})
