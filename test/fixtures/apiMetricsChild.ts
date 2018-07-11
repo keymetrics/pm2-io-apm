9e48c3813139295a831e51d8034a12db06f4d92b
vince <wallet77@gmail.com>
2018-07-11 18:00:56 +0200

    test: align test with unref timers on node 4/6

--- test/fixtures/apiMetricsChild.ts
+++ test/fixtures/apiMetricsChild.ts
@@ -37,3 +37,10 @@ allMetrics.metricHistogram.update(10)
 // test inline declaration
 const metric = io.metric('metricInline')
 metric.set(11)
+
+// set something into event loop. Else test will exit immediately
+const timer = setInterval(function () {}, 5000)
+
+process.on('SIGINT', function () {
+  clearInterval(timer)
+})
--- test/fixtures/apiTransposeBackwardChild.ts
+++ test/fixtures/apiTransposeBackwardChild.ts
@@ -7,3 +7,10 @@ pmx.init()
 pmx.probe().transpose('transpose', function () {
   return 'transposeResponse'
 })
+
+// set something into event loop. Else test will exit immediately
+const timer = setInterval(function () {}, 5000)
+
+process.on('SIGINT', function () {
+  clearInterval(timer)
+})
