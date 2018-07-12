714a6ae437e550bcc9b9a30112bb2ed8f7b83217
vince <wallet77@gmail.com>
2018-07-12 18:10:16 +0200

    refactor: #145 use event-loop-inspector as native module

--- test/actions/eventLoopInspector.spec.ts
+++ test/actions/eventLoopInspector.spec.ts
@@ -4,22 +4,10 @@ import { fork, exec } from 'child_process'
 import Inspector from '../../src/actions/eventLoopInspector'
 import Action from '../../src/features/actions'
 
-const MODULE = 'event-loop-inspector'
-
 describe('EventLoopInspector', function () {
   this.timeout(50000)
 
   describe('Event loop inspector module', function () {
-    before(function (done) {
-      exec('npm install ' + MODULE, function (err) {
-        expect(err).to.equal(null)
-        setTimeout(done, 1000)
-      })
-    })
-
-    after(function (done) {
-      exec('npm uninstall ' + MODULE, done)
-    })
 
     it('should get event loop data', (done) => {
       const child = fork(SpecUtils.buildTestPath('fixtures/actions/eventLoopInspectorChild.js'))
@@ -43,38 +31,6 @@ describe('EventLoopInspector', function () {
       setTimeout(function () {
         child.send('km:event-loop-dump')
       }, 2000)
-    })
-  })
-
-  describe('Event loop inspector module not install', function () {
-
-    before(function (done) {
-      exec('npm uninstall ' + MODULE, done)
-    })
-
-    it('should display error message cause module is not present', async () => {
-      const action = new Action()
-      action.init()
-
-      const eventLoopInspector = new Inspector(action)
-
-      await eventLoopInspector.init().catch((e) => {
-        expect(e.message).to.equal('event-loop-inspector not found')
-      })
-    })
-
-    it('should catch error cause module is not present', async () => {
-      const action = new Action()
-      action.init()
-
-      const eventLoopInspector = new Inspector(action)
-
-      try {
-        await eventLoopInspector.init()
-      } catch (e) {
-        assert.fail(e)
-      }
-
     })
   })
 })
--- test/fixtures/actions/eventLoopInspectorChild.ts
+++ test/fixtures/actions/eventLoopInspectorChild.ts
@@ -6,3 +6,11 @@ action.init()
 
 const eventLoopInspector = new Inspector(action)
 eventLoopInspector.init()
+
+// set something into event loop. Else test will exit immediately
+const timer = setInterval(function () {}, 5000)
+
+process.on('SIGINT', function () {
+  action.destroy()
+  clearInterval(timer)
+})
