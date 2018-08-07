be228ac89ce676209741a08d841e23878fe9b6b0
vmarchaud <contact@vmarchaud.fr>
2018-08-07 17:15:17 +0200

    feat: add scopes & async stack trace on error

--- test/fixtures/features/catchAllInspectorChild.ts
+++ test/fixtures/features/catchAllInspectorChild.ts
@@ -1,23 +1,15 @@
+'use strict'
+
 import { NotifyFeature } from '../../../src/features/notify'
-import * as express from 'express'
-const app = express()
-const http = require('http').Server(app)
 
-function breakApp () {
-  throw new Error('test')
-}
-
-app.get('/', function (req, res) {
+const http = require('http').Server((req, res) => {
   const toto = 'yolo'
-  timer = setTimeout(function () {
-    const result = breakApp()
-  }, 100)
+  // send doesnt exist
   res.send('ok')
 })
 
 const notify = new NotifyFeature()
 const httpModule = require('http')
-let timer
 let server
 
 notify.init()
@@ -26,6 +18,5 @@ server = http.listen(3001, function () {
 })
 
 process.on('SIGINT', function () {
-  clearInterval(timer)
   server.close()
 })
