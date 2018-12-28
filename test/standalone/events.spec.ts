824c00ad727dbaac26fbc695ea24267a3fd919b2
vmarchaud <contact@vmarchaud.fr>
2018-12-28 19:43:43 +0100

    fix: correctly format event packet in standalone mode

---
+++ test/standalone/events.spec.ts
@@ -0,0 +1,70 @@
+
+import * as assert from 'assert'
+import 'mocha'
+import * as semver from 'semver'
+import * as io from '../../src'
+// install patch before requiring the helpers
+io.init()
+import { WSServer, HandshakeServer } from './helper'
+
+if (semver.satisfies(process.version, '< 6')) {
+  console.log('standalone test need to be ran under at least node 6')
+  process.exit(0)
+}
+
+describe('Standalone Tracing', function () {
+  this.timeout(10000)
+  let httpServer
+  let wsServer
+  let apm
+
+  before(() => {
+    httpServer = new HandshakeServer()
+    wsServer = new WSServer()
+  })
+
+  after(() => {
+    io.destroy()
+    httpServer.destroy()
+    wsServer.destroy()
+  })
+
+  it('should init agent', () => {
+    process.env.KEYMETRICS_NODE = 'http://localhost:5934'
+    io.init({
+      standalone: true,
+      apmOptions: {
+        publicKey: 'aa',
+        secretKey: 'bb',
+        appName: 'service'
+      }
+    })
+  })
+
+  it('should receive status', (done) => {
+    wsServer.once('message', (data) => {
+      const packet = JSON.parse(data)
+      assert(packet.channel === 'status')
+      return done()
+    })
+  })
+
+  it('should send event and receive data', (done) => {
+    wsServer.on('message', (data) => {
+      const packet = JSON.parse(data)
+      if (packet.channel !== 'human:event') return
+      assert(typeof packet.payload.name === 'string')
+      assert(typeof packet.payload.data.custom === 'number')
+      assert(typeof packet.payload.data.nested === 'object')
+      wsServer.removeAllListeners()
+      return done()
+    })
+    io.emit('test', {
+      custom: 1,
+      number: 2,
+      nested: {
+        afrg: 2
+      }
+    })
+  })
+})
