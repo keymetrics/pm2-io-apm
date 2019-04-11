015bc1b942a890289667e1800bbb41947db37d1f
vmarchaud <contact@vmarchaud.fr>
2019-04-11 14:00:37 +0200

    tests: fix new node types

--- test/fixtures/metrics/tracingChild.ts
+++ test/fixtures/metrics/tracingChild.ts
@@ -9,6 +9,7 @@ pmx.init({
 // @ts-ignore added in ci only
 import * as express from 'express'
 import { SpanKind } from '@opencensus/core'
+import { AddressInfo } from 'net'
 const app = express()
 
 const http = require('http')
@@ -18,7 +19,7 @@ const https = require('https')
 let timer
 
 app.get('/', function (req, res) {
-  http.get('http://localhost:' + server.address().port + '/toto', (_) => {
+  http.get('http://localhost:' + (server.address() as AddressInfo).port + '/toto', (_) => {
     const tracer = pmx.getTracer()
     if (tracer === undefined) throw new Error('tracer undefined')
     const customSpan = tracer.startChildSpan('customspan', SpanKind.CLIENT)
@@ -36,7 +37,7 @@ app.get('/toto', function (req, res) {
 
 const server = app.listen(3001, function () {
   timer = setTimeout(function () {
-    http.get('http://localhost:' + server.address().port, (_) => {
+    http.get('http://localhost:' + (server.address() as AddressInfo).port, (_) => {
       return
     })
     https.get('https://google.fr')
