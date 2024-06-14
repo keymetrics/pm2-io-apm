57ecda6eb5b59f88091897860c9eb0e34439b4a2
skypesky <2565978507@qq.com>
2024-06-14 17:51:44 +0800

    feat: add disable rule

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,19 @@
-import * as semver from 'semver'
+import * as semver from 'semver';
 
 export default {
-  METRIC_INTERVAL: 990
-}
+  METRIC_INTERVAL: 990,
+};
 
-export function canUseInspector () {
-  const isAboveNode10 = semver.satisfies(process.version, '>= 10.1.0')
-  const isAboveNode8 = semver.satisfies(process.version, '>= 8.0.0')
-  const canUseInNode8 = process.env.FORCE_INSPECTOR === '1'
-    || process.env.FORCE_INSPECTOR === 'true' || process.env.NODE_ENV === 'production'
-  return isAboveNode10 || (isAboveNode8 && canUseInNode8)
+export function canUseInspector() {
+  if (process.env.DISABLE_PM2_IO === 'true') {
+    return false;
+  }
+
+  const isAboveNode10 = semver.satisfies(process.version, '>= 10.1.0');
+  const isAboveNode8 = semver.satisfies(process.version, '>= 8.0.0');
+  const canUseInNode8 =
+    process.env.FORCE_INSPECTOR === '1' ||
+    process.env.FORCE_INSPECTOR === 'true' ||
+    process.env.NODE_ENV === 'production';
+  return isAboveNode10 || (isAboveNode8 && canUseInNode8);
 }
