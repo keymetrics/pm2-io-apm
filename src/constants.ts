714ec396ca212e43e6129a6e2413b2be2c1b1aa1
Unitech <asz@pm2.io>
2024-11-12 14:10:53 +0000

    6.1.0: skip inspector - bun support

--- src/constants.ts
+++ src/constants.ts
@@ -5,9 +5,12 @@ export default {
 }
 
 export function canUseInspector () {
+  // @ts-ignore
+  const isBun = typeof Bun !== 'undefined'
   const isAboveNode10 = semver.satisfies(process.version, '>= 10.1.0')
   const isAboveNode8 = semver.satisfies(process.version, '>= 8.0.0')
   const canUseInNode8 = process.env.FORCE_INSPECTOR === '1'
-    || process.env.FORCE_INSPECTOR === 'true' || process.env.NODE_ENV === 'production'
-  return isAboveNode10 || (isAboveNode8 && canUseInNode8)
+        || process.env.FORCE_INSPECTOR === 'true' || process.env.NODE_ENV === 'production'
+
+  return !isBun && (isAboveNode10 || (isAboveNode8 && canUseInNode8))
 }
